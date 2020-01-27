c10c6b44a381f8ddd134fd3c0d18ed9a67eb8ac3
Stanisław Chmiela <sjchmiela@gmail.com>
2020-01-27 17:33:34 +0100

    [expo-notifications] Implement badge counters in TS

--- packages/expo-notifications/build/index.d.ts
+++ packages/expo-notifications/build/index.d.ts
@@ -1,6 +1,8 @@
 export { default as getDevicePushTokenAsync } from './getDevicePushTokenAsync';
 export { default as getExpoPushTokenAsync } from './getExpoPushTokenAsync';
 export { default as presentNotificationAsync } from './presentNotificationAsync';
+export { default as getBadgeCountAsync } from './getBadgeCountAsync';
+export { default as setBadgeCountAsync } from './setBadgeCountAsync';
 export * from './TokenEmitter';
 export * from './NotificationsEmitter';
 export * from './NotificationsHandler';
