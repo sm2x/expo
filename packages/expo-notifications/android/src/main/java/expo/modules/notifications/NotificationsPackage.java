ef950ee12bb92ccc6bcade999870cd4808266bdd
Stanisław Chmiela <sjchmiela@gmail.com>
2020-02-17 19:33:51 +0100

    [expo-notifications] Remove notification builder factory

--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/NotificationsPackage.java
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/NotificationsPackage.java
@@ -4,28 +4,20 @@ import android.content.Context;
 
 import org.unimodules.core.BasePackage;
 import org.unimodules.core.ExportedModule;
-import org.unimodules.core.interfaces.InternalModule;
 import org.unimodules.core.interfaces.SingletonModule;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import expo.modules.notifications.installationid.InstallationIdProvider;
 import expo.modules.notifications.notifications.NotificationManager;
 import expo.modules.notifications.notifications.emitting.NotificationsEmitter;
 import expo.modules.notifications.notifications.handling.NotificationsHandler;
-import expo.modules.notifications.notifications.presentation.ExpoNotificationBuilderFactory;
 import expo.modules.notifications.permissions.NotificationPermissionsModule;
 import expo.modules.notifications.tokens.PushTokenManager;
 import expo.modules.notifications.tokens.PushTokenModule;
 
 public class NotificationsPackage extends BasePackage {
-  @Override
-  public List<InternalModule> createInternalModules(Context context) {
-    return Collections.singletonList((InternalModule) new ExpoNotificationBuilderFactory());
-  }
-
   @Override
   public List<ExportedModule> createExportedModules(Context context) {
     return Arrays.asList(
